2f430fc78dd6beadd6a62c0b59ab78749f0abfe5
Md. Mahbubur rahman <mahbubur001@gmail.com>
2019-10-18 16:39:20 +0600

    Updated

--- lib/classes/TLPTeamTemplate.php
+++ lib/classes/TLPTeamTemplate.php
@@ -1,42 +1,39 @@
 <?php
 
-if(!class_exists('TLPTeamTemplate')):
+if ( ! class_exists( 'TLPTeamTemplate' ) ):
 
-    /**
-     *
-     */
-    class TLPTeamTemplate
-    {
+	/**
+	 *
+	 */
+	class TLPTeamTemplate {
 
-        function __construct()
-        {
-            add_filter( 'template_include', array( $this, 'template_loader' ) );
-        }
+		function __construct() {
+			add_filter( 'template_include', array( $this, 'template_loader' ) );
+		}
 
-        public static function template_loader( $template ) {
-            // $file = array('single-team.php');
-            $find = array();
-            $file = null;
-            global $TLPteam;
-            if ( is_single() && get_post_type() == $TLPteam->post_type ) {
+		public static function template_loader( $template ) {
+			$find = array();
+			$file = null;
+			global $TLPteam;
+			if ( is_single() && get_post_type() == $TLPteam->post_type ) {
 
-                $file 	= 'single-team.php';
-                $find[] = $file;
-                $find[] = $TLPteam->templatesPath . $file;
+				$file   = 'single-team.php';
+				$find[] = $file;
+				$find[] = $TLPteam->templatesPath . $file;
 
-            }
+			}
 
-            if ( @$file ) {
+			if ( $file ) {
 
-                $template = locate_template( array_unique( $find ) );
-                if ( ! $template ) {
-                    $template = $TLPteam->templatesPath  . $file;
-                }
-            }
+				$template = locate_template( array_unique( $find ) );
+				if ( ! $template ) {
+					$template = $TLPteam->templatesPath . $file;
+				}
+			}
 
-            return $template;
-        }
+			return $template;
+		}
 
-    }
+	}
 
 endif;
